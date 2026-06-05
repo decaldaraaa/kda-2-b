5a65c103f1da5f80a614bf2ab93cf0c6d74ec6f2
decaldaraaa <kuntohidayat20@student.uns.ac.id>
2026-06-05 13:02:47 +0700

    backend terakhir

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Sistem Autentikasi Dinamis")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
